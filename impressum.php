5ab43a7c08304b81d8cd2b7f6009003debd317b2
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-11-28 20:54:19 +0100

    Add datenschutz, impressum and discplaimer

---
+++ impressum.php
@@ -0,0 +1,25 @@
+<?php require_once("templates/header.php"); ?>
+<div class="container py-3">
+    <div>
+        <h1 class="display-3 text-center text-kolping-orange">Impressum</h1>
+        <h5 class="mb-1">Angaben gemäß § 5 Telemediengesetz (TMG):</h5>
+        <p class="mb-3">
+            Jan Schniebs<br>
+            Ludwigstraße 25<br>
+            73614 Schorndorf<br>
+            E-Mail: <a href="mailto:jan@schniebs.com" class="link">jan@schniebs.com</a><br>
+            Telefon: +49 7181 9379852
+        </p>
+        <h5 class="mb-1">Inhaltlich verantwortlich i.S.v. §55 Abs. 2 RStV:</h5>
+        <p class="mb-3">
+            Jan Schniebs<br>
+            Ludwigstraße 25<br>
+            73614 Schorndorf<br>
+            E-Mail: <a href="mailto:jan@schniebs.com" class="link">jan@schniebs.com</a><br>
+            Telefon: +49 7181 9379852<br>
+            <br>
+            Nähere Hinweise zu unserem Datenschutz finden Sie unter diesem <a href="/datenschutz.php" class="link">Link</a>.
+        </p>
+    </div>
+</div>
+<?php require_once("templates/footer.php"); ?>
